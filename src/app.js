cc9991f5ae3c34e1076212fdded5a046cf26a718
Vagner Bruno <vagnerbpb@gmail.com>
2023-04-10 18:00:44 -0300

    PREPAREI A ESTRUTURA DO SWAGGER PARA INICIAR A DOCUMENTAÇÃO

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ require('dotenv').config()
 const bodyParser = require('body-parser')
 const express = require("express")
 const app = express()
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.json')
 const router = require("./routes/routes")
 const cors = require('cors')
 
 app.use(cors())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
  
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
