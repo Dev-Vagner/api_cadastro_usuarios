e514fcca9e439ec8b2eedf7efcc809682064f972
Vagner Bruno <vagnerbpb@gmail.com>
2023-04-12 11:21:13 -0300

    FINALIZEI A VERIFICAÇÃO DA  DOCUMENTAÇÃO DA API DE CADASTRO DE USUÁRIOS

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,14 +4,14 @@ const HomeController = require('../controllers/HomeController')
 const UserController = require("../controllers/UserController")
 const UserLogged = require('../middleware/UserLogged')
 
-router.get('/', HomeController.index) // OK
-router.post('/user', UserController.create) //OK
+router.get('/', HomeController.index)
+router.post('/user', UserController.create)
 router.get('/users', UserLogged, UserController.findAllUsers)
-router.get('/user/:id', UserLogged, UserController.findUser) //OK
-router.put('/user/:id', UserLogged, UserController.edit) //OK
-router.delete('/user/:id', UserLogged, UserController.delete) //OK
-router.post('/passwordToken/:userId', UserLogged, UserController.sendPasswordToken) //OK
-router.put('/password/:userId', UserLogged, UserController.changePassword) //OK
-router.post('/login', UserController.login) //OK
+router.get('/user/:id', UserLogged, UserController.findUser)
+router.put('/user/:id', UserLogged, UserController.edit)
+router.delete('/user/:id', UserLogged, UserController.delete)
+router.post('/passwordToken/:userId', UserLogged, UserController.sendPasswordToken)
+router.put('/password/:userId', UserLogged, UserController.changePassword)
+router.post('/login', UserController.login)
 
 module.exports = router
